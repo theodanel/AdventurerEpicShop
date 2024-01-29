237fa0cc62cb310d1832fba01cf10f0301807ebf
Axel Demeyere <axel.demeyere59@gmail.com>
2024-01-29 11:08:03 +0100

    ADD Routes & FIX video

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import logo from '../logo.png';
 import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
+    const navigate = useNavigate()
+
     return (
         <header className="App-header">
             <div className='logo-buttons'>
-                <div className='header-empty-space'> </div>
-            <img src={logo} className="App-logo" alt="logo" />
-                <div className='header-empty-space2'> </div>
+            <img onClick={() => navigate('/')} src={logo} className="App-logo" alt="logo" />
                     <div className='headerButtons'>
-                        <Button> Potions</Button>
-                        <Button> Armes</Button>
-                        <Button> Armures</Button>
-                        <Button> Montures</Button>
+                        <Button onClick={() => navigate('/potions')} > Potions </Button>
+                        <Button onClick={() => navigate('/armes')} > Armes </Button>
+                        <Button onClick={() => navigate('/armures')} > Armures </Button>
+                        <Button onClick={() => navigate('/montures')} > Montures </Button>
                     </div>
              </div>
              <h1 className="App-title">Adventurer's Epic Shop</h1>
---
+++ src/components/Potions.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+function Potions () {
+    return (
+        <>
+        <Header />
+        <p>
+            Toutes nos potions
+        </p></>
+        
+    )
+}
+
+export default Potions
